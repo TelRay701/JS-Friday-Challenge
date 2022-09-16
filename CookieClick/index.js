4b9863ef11355553109934efd2f32f1cdbe6b7e3
Ray T <telray701@gmail.com>
2022-09-16 14:52:20 -0400

    Initial commit

---
+++ CookieClick/index.js
@@ -0,0 +1,11 @@
+var clicked = 0;
+var autoClicks = 0;
+
+function up() {
+  clicked = clicked + 1;
+  counter.innerHTML = clicked;
+}
+
+document.querySelector(".upgradebtn").addEventListener("click", function {
+    if ();
+})
